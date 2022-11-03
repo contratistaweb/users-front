b8592646ede52a9a4b2e92b6d3a2c01fe79c0a9a
Luillin Escobar <cataplum.17@hotmail.com>
2022-11-03 00:13:13 -0500

    user module routes configuration

--- src/app/modules/users/users-routing.module.ts
+++ src/app/modules/users/users-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {UsersLayoutComponent} from "./component/users-layout/users-layout.component";
+import {UserListComponent} from "./component/user-list/user-list.component";
+import {UsersFormComponent} from "./component/users-form/users-form.component";
+import {UserDetailsComponent} from "./component/user-details/user-details.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: UsersLayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'list',
+        pathMatch: 'full'
+      },
+      {
+        path: 'list',
+        component: UserListComponent
+      },
+      {
+        path: 'action',
+        component: UsersFormComponent
+      },
+      {
+        path: 'details',
+        component: UserDetailsComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
