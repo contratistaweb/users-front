c53d063e17208f9c8f5b98e7e774c20ae7d1f5b0
Luillin Escobar <cataplum.17@hotmail.com>
2022-11-03 00:07:52 -0500

    configuration initial path of the application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
+import {MainLayoutComponent} from "./modules/core/shared/componentes/main-layout/main-layout.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'users',
+        pathMatch: 'full'
+      },
+      {
+        path: '',
+        loadChildren: () => import('./modules/users/users.module').then(module => module.UsersModule)
+      }
+    ]
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: PreloadAllModules,
+    useHash: true
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
